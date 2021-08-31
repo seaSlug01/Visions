4598d953d21ecdc8a87661f8b4a9220e07db96c0
Kwstas <deekwstaz@gmail.com>
2021-08-31 16:32:18 +0300

    Made FAQ Section component

---
+++ src/components/FaqSection.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+function FaqSection() {
+  return (
+    <div className='faq'>
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className='question'>
+        <h4>How Do I Start?</h4>
+        <div className='answer'>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, sit.
+          </p>
+        </div>
+      </div>
+      <div className='question'>
+        <h4>Daily Schedule</h4>
+        <div className='answer'>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Consequuntur, necessitatibus!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, sit.
+          </p>
+        </div>
+      </div>
+      <div className='question'>
+        <h4>Payments</h4>
+        <div className='answer'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            doloremque quisquam consequuntur explicabo magni sit laboriosam
+            animi facilis, numquam tenetur tempora labore quia nisi minus
+            architecto dolores quasi quae illum, eos quibusdam officiis illo
+            delectus, atque fuga! Dolorem quia aspernatur asperiores voluptas
+            earum distinctio optio sint! Ipsum possimus facere velit!
+          </p>
+        </div>
+      </div>
+      <div className='question'>
+        <h4>What kind of sessions are available?</h4>
+        <div className='answer'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            doloremque quisquam consequuntur explicabo magni sit laboriosam
+            animi facilis, numquam tenetur tempora labore quia nisi minus
+            architecto dolores quasi quae illum, eos quibus!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FaqSection;
